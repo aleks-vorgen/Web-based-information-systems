104ece65aca3ad9309569604921efe24165eeb1d
ALexus <vorgen.morgen@yandex.ru>
2023-12-08 15:06:39 +0200

    lab8

--- lab8/mvc/model/Model.php
+++ lab8/mvc/model/Model.php
@@ -1,26 +1,22 @@
 <?php
 include_once("model/Book.php");
 
-class Model
-{
-  private $db;
+class Model {
+  private PDO $db;
 
-  public function __construct()
-  {
+  public function __construct() {
     $this->db = new PDO('pgsql:host=localhost;dbname=postgres', 'postgres', '0000');
     $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 
-  public function getBookList()
-  {
+  public function getBookList(): bool|array {
     $stmt = $this->db->query('SELECT books.title, books.description, books.publication_year, books.page_count, books.cover_photo, authors.last_name, authors.first_name 
                                   FROM books 
                                   JOIN authors ON books.author_id = authors.id');
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  public function searchBooks($author, $title)
-  {
+  public function searchBooks($author, $title): bool|array {
       $query = 'SELECT books.title, books.description, books.publication_year, books.page_count, books.cover_photo, authors.last_name, authors.first_name 
                   FROM books 
                   JOIN authors ON books.author_id = authors.id 
@@ -32,5 +28,4 @@ class Model
   
       return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
-  
 }
